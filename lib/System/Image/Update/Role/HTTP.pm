734c0163c969ba303483b97646a79cb615b12f5d
Jens Rehsack <sno@netbsd.org>
2014-09-29 19:32:06 +0200

    fix bogus local variables used to hold idents

--- lib/System/Image/Update/Role/HTTP.pm
+++ lib/System/Image/Update/Role/HTTP.pm
@@ -1,5 +1,9 @@
 package System::Image::Update::Role::HTTP;
 
+use 5.014;
+use strict;
+use warnings FATAL => 'all';
+
 use Moo::Role;
 use Net::Async::HTTP;
 use URI;
@@ -20,7 +24,7 @@ my $http_user_built;
 sub _build_http_user
 {
     my $eth0_info = qx(ip link show dev eth0);
-    my $http_user_built = $eth0_info =~ m,link/ether\s((?:[a-f0-9]{2}:){7}[a-f0-9]{2}),ms ? $1 : "";
+    ( $http_user_built = $eth0_info =~ m,link/ether\s((?:[a-f0-9]{2}:){5}[a-f0-9]{2}),ms ? $1 : "" ) =~ s/://g;
     $http_user_built;
 }
 
@@ -31,7 +35,7 @@ my $http_passwd_built;
 sub _build_http_passwd
 {
     my $eth0_info = qx(ip link show dev eth0);
-    my $http_passwd_built = $eth0_info =~ m,link/ether\s((?:[a-f0-9]{2}:){7}[a-f0-9]{2}),ms ? $1 : "";
+    ( $http_passwd_built = $eth0_info =~ m,link/ether\s((?:[a-f0-9]{2}:){5}[a-f0-9]{2}),ms ? $1 : "" ) =~ s/://g;
     $http_passwd_built;
 }
 
@@ -40,8 +44,11 @@ around collect_savable_config => sub {
     my $self                   = shift;
     my $collect_savable_config = $self->$next(@_);
 
-    $http_user_built   or $collect_savable_config->{http_user}   = $self->http_user;
-    $http_passwd_built or $collect_savable_config->{http_passwd} = $self->http_passwd;
+    my $http_user   = $self->http_user;
+    my $http_passwd = $self->http_passwd;
+
+    defined $http_user_built   or $collect_savable_config->{http_user}   = $self->http_user;
+    defined $http_passwd_built or $collect_savable_config->{http_passwd} = $self->http_passwd;
 
     $collect_savable_config;
 };
