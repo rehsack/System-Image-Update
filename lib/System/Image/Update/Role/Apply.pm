8428a6de6439719129f337ea85dcdf2ca683705e
Jens Rehsack <sno@netbsd.org>
2015-02-03 09:49:18 +0100

    mark a dangerous sequence for known use-case

--- lib/System/Image/Update/Role/Apply.pm
+++ lib/System/Image/Update/Role/Apply.pm
@@ -81,6 +81,7 @@ sub _apply4real
     $self->reset_config;
     $self->save_config;
 
+    # XXX svc -t (hp2sm) will abort flash!
     system( $self->flash_command ) or return $self->log->error("Cannot send execute flash command: $!");
 }
 
