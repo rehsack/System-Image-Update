737a83a5c85b0dd712103d511b91f6894ad22081
Jens Rehsack <sno@netbsd.org>
2015-03-19 09:01:15 +0100

    bump copyright year to 2015

--- lib/System/Image/Update/Role/Prove.pm
+++ lib/System/Image/Update/Role/Prove.pm
@@ -132,7 +132,7 @@ Jens Rehsack, C<< <rehsack at cpan.org> >>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 Jens Rehsack.
+Copyright 2014-2015 Jens Rehsack.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the the Artistic License (2.0). You may obtain a
